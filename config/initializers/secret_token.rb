a53ea1fda6c82156e258aa7453edd26ceebe0606
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-01-29 16:01:20 -0800

    Another rails blog, here are the defaults

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ashleigh::Application.config.secret_token = 'ef84bdb0266c531e300538de46eb408b38e8561301faadb37f5fa97695ac50af68cf3bce0c289a9272e862df0d4248db2461b07ec0d526c91189eaa6c617f8d7'
