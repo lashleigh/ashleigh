50a578b24552c0e2dda3bea56f74f2cea0ffb543
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-02-06 15:04:33 -0800

    Generated a notes controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ashleigh::Application.routes.draw do
+  get "note/index"
+
   get "log_in" => "sessions#new", :as => "log_in"  
   get "log_out" => "sessions#destroy", :as => "log_out"  
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110202025737) do
+ActiveRecord::Schema.define(:version => 20110206230211) do
+
+  create_table "notes", :force => true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
