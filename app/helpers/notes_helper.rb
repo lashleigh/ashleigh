21f612bd32d0dd770515368898a36a7a54e5905e
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-02-07 16:09:25 -0800

    Somewhat improved formatting

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -3,6 +3,14 @@ module NotesHelper
     "width:#{n.width}px;"+"height:#{n.height}px;"
   end
 
+  def content_style(n)
+    "width:#{n.width-10}px;"+"height:#{n.height-10}px;"
+  end
+
+  def form_style(n)
+    "display:none;"+"width:#{n.width-10}px;"+"height:#{n.height-10}px;"
+  end
+
   def draggable_style(n)
     "top:#{n.top}px;"+"left:#{n.left}px;"
   end
--- public/javascripts/note.js
+++ public/javascripts/note.js
@@ -7,18 +7,22 @@ $(function() {
     }
   })
   $( ".resizable" ).resizable({
+    resize: function(event, ui) {
+      $(this).find('.formatted_content').css("width",(ui.size.width-10)+"px");
+      $(this).find('.formatted_content').css("height",(ui.size.height-10)+"px");
+    },
     stop: function(event, ui) {
       $.get("/dimension", {id: parseInt($(this).parent().attr("id").split("_")[1]), width: $(this).width(), height: $(this).height() })
     }
   })
 
   $(".resizable").dblclick( function() {
-    var outer_height = $(this).css("height");
-    var outer_width = $(this).css("width");
-    $(this).find(".edit_note").find("textarea").css("height", outer_height);
-    $(this).find(".edit_note").find("textarea").css("width", outer_width);
-    $(this).find(".edit_note").css("height", outer_height);
-    $(this).find(".edit_note").css("width", outer_width);
+    var outer_height = $(this).height() - 15;
+    var outer_width = $(this).width() - 15;
+    $(this).find(".edit_note").find("textarea").css("height", outer_height+"px");
+    $(this).find(".edit_note").find("textarea").css("width", outer_width+"px");
+    //$(this).find(".edit_note").css("height", outer_height);
+    //$(this).find(".edit_note").css("width", outer_width);
     $(this).find(".edit_note").show();
     $(this).find(".edit_note").find("textarea").focus();
     $(this).find(".formatted_content").hide();
@@ -33,8 +37,5 @@ $(function() {
     $('.edit_note').next().show();
   });
 
-  $('body').dblclick( function() {
-      $.get("/notes/new");
-  });
 });
 
