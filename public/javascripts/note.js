eaf1ade5c51b754929759aeffbe4b57eb623e498
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-02-07 20:38:07 -0800

    The livequery plugin made it so newly created notes also get the proper draggable and resizeable behaviour.

--- public/javascripts/note.js
+++ public/javascripts/note.js
@@ -1,21 +1,25 @@
 $(function() {
-  $( ".draggable" ).draggable({ 
-    snap: ".draggable, .slide",
-    stop: function(event, ui) {
-      var current = $(this)
-      $.get("/position", {id: parseInt(current.attr("id").split("_")[1]), ttop: current.position().top, left: current.position().left })
-    }
-  })
-  $( ".resizable" ).resizable({
-    containment: '.slide',
-    resize: function(event, ui) {
-      $(this).find('.formatted_content').css("width",(ui.size.width-10)+"px");
-      $(this).find('.formatted_content').css("height",(ui.size.height-10)+"px");
-    },
-    stop: function(event, ui) {
-      $.get("/dimension", {id: parseInt($(this).parent().attr("id").split("_")[1]), width: $(this).width(), height: $(this).height() })
-    }
-  })
+  $(".draggable").livequery( function() {
+    $(this).draggable({ 
+      snap: ".draggable, .slide",
+      stop: function(event, ui) {
+        var current = $(this)
+        $.get("/position", {id: parseInt(current.attr("id").split("_")[1]), ttop: current.position().top, left: current.position().left })
+      }
+    });
+  });
+  $(".resizable").livequery( function() {
+    $(this).resizable({
+      containment: '.slide',
+      resize: function(event, ui) {
+        $(this).find('.formatted_content').css("width",(ui.size.width-10)+"px");
+        $(this).find('.formatted_content').css("height",(ui.size.height-10)+"px");
+      },
+      stop: function(event, ui) {
+        $.get("/dimension", {id: parseInt($(this).parent().attr("id").split("_")[1]), width: $(this).width(), height: $(this).height() })
+      }
+    });
+  });
 
   $(".resizable").live("mouseover", function() {
     $(this).find(".delete").show();
@@ -23,7 +27,7 @@ $(function() {
   $(".resizable").live("mouseout", function() {
     $(".delete").hide();
   });
-  $(".resizable").dblclick( function() {
+  $(".resizable").live("dblclick", function() {
     var outer_height = $(this).height() - 15;
     var outer_width = $(this).width() - 15;
     $(this).find(".edit_note").find("textarea").css("height", outer_height+"px");
@@ -33,7 +37,7 @@ $(function() {
     $(this).find(".formatted_content").hide();
   });
     
-  $(".resizable").focusout( function() {
+  $(".resizable").live("focusout", function() {
     var self = this;
     $.get("/update", $(this).find(".edit_note").serialize(), function(result, txtstatus) {
       $(self).find('.formatted_content').html(result);
@@ -44,7 +48,6 @@ $(function() {
 
   $(".delete").live("click", function(event) {
     var self = $(this).attr("id").split("_")[1];
-    console.log(self);
     $.get("/destroy", {id: self}, function(result, txtstatus) {
       $("#note_"+self).hide();   
     });
